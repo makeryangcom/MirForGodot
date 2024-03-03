2c392f75907bebe9575fc56fa511900a1370dab9
GeekROS <15300555770>
2024-03-04 00:05:39 +0800

    1.0.0

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
 
-scp -r ./engine/release/server* root@172.21.255.206:/data/wwwroot/game/
+scp -r ./engine/release/server* root@172.21.254.81:/data/wwwroot/game/
 
-ssh root@172.21.255.206 "chmod +x /data/wwwroot/game/* && cd /opt/Engine2D && sudo ./tools/update.sh"
+ssh root@172.21.254.81 "chmod +x /data/wwwroot/game/* && cd /opt/Engine2D && sudo ./tools/update.sh"
