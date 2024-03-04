c7da010f019e0e6875f0119918d4effc4c357e24
makeryangcom <open@wileho.com>
2024-03-04 17:46:15 +0800

    1.0.0

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
 
-scp -r ./engine/release/server* root@172.21.254.81:/data/wwwroot/game/
+scp -r ./engine/release/server* root@172.21.245.0:/data/wwwroot/game/
 
-ssh root@172.21.254.81 "chmod +x /data/wwwroot/game/* && cd /opt/Engine2D && sudo ./tools/update.sh"
+ssh root@172.21.245.0 "chmod +x /data/wwwroot/game/* && cd /opt/Engine2D && sudo ./tools/update.sh"
